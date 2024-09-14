d155250b962039a835212d4e68c75dbb81ed1f88
sakshammaithani <151917211+sakshammaithani@users.noreply.github.com>
2024-09-14 20:08:40 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String args[])
+    
+    {
+        String firstname = "Saksham";
+        String lastname = "Maithani";
+        String fullname = firstname + lastname ;
+        System.out.println(fullname);
+
+{
+    int a = 5 ;
+    int b = 8 ;
+    System.out.println(a+b);
+    
+  
+    System.out.println(a+b);
+
+}
+        {
+            System.out.println("Student Database:");
+            int studentId=2211;
+            String studentName="Sam";
+            int studentAge=19;
+            int studentClass=10;
+            float studentPercentage=80.5f;
+
+
+            System.out.println("studentId:"+studentId);
+            System.out.println("studentName:"+studentName);
+            System.out.println("studentAge:"+studentAge);
+            System.out.println("studentClass:"+studentClass);
+            System.out.println("studentPercentage"+studentPercentage);
+
+
+
+            
+
+        }
+    }
+}
---
+++ sam1.java
@@ -0,0 +1,25 @@
+public class sam1 {
+    public static void main(String[] args) {
+        //program for area of rectangle
+        int length =8;
+        int width = 10;
+        int area;
+
+        area=length*width;
+        System.out.println("length:"+length);
+        System.out.println("width:"+width);
+        System.out.println("area:"+area);
+    
+    {
+        //program for calculate total cost of items 
+         int items=50;
+         float costperitem=9.98f;
+         float totalcost;
+
+         totalcost=items*costperitem;
+
+         System.out.println("total items:"+items);
+         System.out.println("costperitem:"+costperitem);
+         System.out.println("totalcost:"+totalcost);
+    }}
+}
